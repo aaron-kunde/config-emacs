660817547762a7bd569041c6ca8e4dd3e80ffce6
Aaron Kunde <aaron.kunde@adesso.de>
2024-05-19 13:09:42 +0200

    Add customization of UI

--- init.el
+++ init.el
@@ -4,3 +4,6 @@
 (setq column-number-mode t)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (setq size-indication-mode t)
+(set-face-background 'default "black")
+(set-face-foreground 'default "white")
+(set-face-background 'cursor "green")
