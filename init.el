cf0fe7e030c5517c6af007b7d718aa47a45254ba
Aaron Kunde <aaron.kunde@adesso.de>
2024-05-19 13:09:00 +0200

    Add customization of basic functionalities

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
 (setq inhibit-startup-screen t)
+(setq ring-bell-function 'ignore)
+(tool-bar-mode -1)
+(setq column-number-mode t)
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(setq size-indication-mode t)
