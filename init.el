5b44f9e76688deffe1b4e9ca8d5e5e815860cff0
Aaron Kunde <aaron.kunde@adesso.de>
2024-06-06 16:23:03 +0200

    Show passprase of GPG in minibuffer

--- init.el
+++ init.el
@@ -18,6 +18,9 @@
 (eval-when-compile (require 'use-package))
 (use-package gnu-elpa-keyring-update
   :config  (setq package-check-signature 'allow-unsigned))
+(use-package epg
+  :custom
+  (epg-pinentry-mode 'loopback))
 (use-package which-key
   :config  (which-key-mode))
 (use-package counsel
